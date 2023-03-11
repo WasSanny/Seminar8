c5bcd1578b7e3477d76f8659c1ec0129380a3c54
Dmitry Vasiliuk <usy222@mail.ru>
2023-03-11 23:19:04 +0300

    коллекции, частотный словарь, сколько раз встречается элемент

---
+++ Task3/Program.cs
@@ -0,0 +1,66 @@
+﻿// Составить частотный словарь элементов двумерного массива.
+// Частотный словарь содержит информацию о том,
+// сколько раз встречается элемент входных данных.
+
+Dictionary<int, int> dictionary = new Dictionary<int, int>();
+
+int[] array = GetArray(25);
+
+PrintArray(array);
+FillDictionary(dictionary, array);
+Console.WriteLine();
+Console.WriteLine();
+PrintDictionary(dictionary);
+
+
+
+int[] GetArray(int length)
+{
+  int[] array = new int[length];
+
+  Random random = new Random();
+
+  for (int i = 0; i < length; i++)
+  {
+    array[i] = random.Next(0, 10);
+  }
+
+  return array;
+}
+
+int ReadInt(string argument)
+{
+  Console.Write($"Input {argument}: ");
+  return int.Parse(Console.ReadLine());
+}
+
+void FillDictionary(Dictionary<int, int> dictionary, int[] array)
+{
+  for (int i = 0; i < array.Length; i++)
+  {
+    if (dictionary.ContainsKey(array[i]))
+    {
+      dictionary[array[i]]++;
+    }
+    else
+    {
+      dictionary.Add(array[i], 1);
+    }
+  }
+}
+
+void PrintDictionary(Dictionary<int, int> dictionary)
+{
+  foreach (var pair in dictionary)
+  {
+    Console.WriteLine($"{pair.Key} ------ {pair.Value}");
+  }
+}
+
+void PrintArray(int[] array)
+{
+  foreach (var pair in array)
+  {
+    Console.Write($"{pair} ");
+  }
+}
